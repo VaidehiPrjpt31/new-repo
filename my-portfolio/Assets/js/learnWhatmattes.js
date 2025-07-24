460333beb31706b48a0502d240926068c92d6092
Vaidehi Prajapati <vaidehi@dolphinwebsolutions.com>
2025-07-24 18:02:51 +0530

    updated

--- my-portfolio/Assets/js/learnWhatmattes.js
+++ my-portfolio/Assets/js/learnWhatmattes.js
@@ -27,8 +27,69 @@ function sco(){
 sco();
 
 
+class IronMan{
+    constructor(name, suitColor , armourStrength){
+            this.name = name;
+            this.suitColor = suitColor;
+            this._armourStrength = armourStrength;
+    }
 
-function changeColor(){
-    
+    fly(){
+        console.log(`${this.name} is flying`);
+    }
+
+    shootLeser(){
+        console.log(`${this.name} is Sooting Leser`);
+    }
+
+    // public method to access the private attribute
+
+    getArmourStrength(){
+        return this._armourStrength;
+    }
+
+    // public method to update the private attribute
+
+    updataeArmourStrength(newStrength){
+        this._armourStrength = newStrength;
+    }
+
 }
 
+const ironMan = new IronMan('Tony Stark' , "Red and Golden" , 100);
+console.log(ironMan.getArmourStrength());
+
+ironMan.updataeArmourStrength(120);
+console.log(ironMan.getArmourStrength());
+
+
+
+// array examples
+
+var nay = ['First' , 'Second'];
+nay.push('Third');
+console.log('Nay : ' , nay );
+
+var a2 = ['Yello' , 'Blue' ,'Gray' , 'Pink' , 'Green'];
+var b2 = ['apple' , 'Blueberry' ,'Graps' , 'Papaya' , 'Gvuava'];
+var c2 = ['Hyundai' , 'Honda'];
+console.log('a2 colors : ' , a2);
+console.log('a2 color slice' , a2.slice());
+console.log('a2 color lenght : ' , a2.length);
+console.log('Colors at :' , a2.at(-1));
+const child = a2.concat(b2);
+const child1 = a2.concat(b2 , c2);
+console.log(child);
+console.log(child1);
+
+let text = b2.constructor;
+console.log(text);
+
+let b2copy = new b2.constructor(...b2);
+console.log(`this is the constructor of the b2 values : ${b2copy}`);
+
+console.log(b2.pop());
+console.log(b2.push("Watermelom"));
+console.log(b2);
+console.log(b2.shift());
+console.log(b2.unshift('apple'));
